3a0e01b88ec80e755f691b21d720d70c46b143f6
Armin <armin.prinz.1990@web.de>
2021-06-14 16:35:12 +0200

    Big update, soundeffects, music etc.

--- Digger/src/mainGame/MainMenu.java
+++ Digger/src/mainGame/MainMenu.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import java.awt.Rectangle;
 import javax.swing.JDesktopPane;
@@ -24,6 +26,7 @@ public class MainMenu {
 	static Font customFontSmall = GameManager.createCustomFont(7f);
 
 	public static void main(String[] args) {
+		SoundManager.playMusic();
 		setFrame();
 	}
 	
@@ -34,7 +37,7 @@ public class MainMenu {
 		mainFrame = new JFrame();
 		mainFrame.setIconImage(icon.getImage());
 		
-		mainFrame.setSize(510, 310);
+		mainFrame.setSize(510, 302);
 		mainFrame.setResizable(false);
 		mainFrame.getContentPane().setLayout(null);
 
@@ -45,7 +48,7 @@ public class MainMenu {
 				g.drawImage(background.getImage(), 0, 0, this);
 			}
 		};
-		desktopPane.setBounds(0, 0, 504, 281);
+		desktopPane.setBounds(0, 0, 504, 273);
 		mainFrame.getContentPane().add(desktopPane);
 		
 		JButton btnStart = new JButton("New Game");
@@ -57,6 +60,11 @@ public class MainMenu {
 			public void actionPerformed(ActionEvent e) {
 				StartGame.main(null);
 				mainFrame.setVisible(false);
+			}
+		});
+		btnStart.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				SoundManager.menuBtnSound();
 			}
 		});
 		btnStart.setBounds(50, 108, 400, 35);
@@ -72,18 +80,43 @@ public class MainMenu {
 				StartGame.loadSaveGame();
 			}
 		});
+		btnContinueGame.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				SoundManager.menuBtnSound();
+			}
+		});
 		desktopPane.add(btnContinueGame);
 		
 		JButton btnHighscore = new JButton("Highscore");
 		btnHighscore.setFont(customFontRegular);
 		btnHighscore.setToolTipText("Show Highscore");
-		btnHighscore.setBounds(50, 200, 400, 35);
+		btnHighscore.setBounds(50, 200, 194, 35);
+		btnHighscore.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				SoundManager.menuBtnSound();
+			}
+		});
 		desktopPane.add(btnHighscore);
+
+		JButton optionsBtn = new JButton("Settings");
+		optionsBtn.setBounds(256, 200, 194, 35);
+		optionsBtn.setFont(customFontRegular);
+		optionsBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuManager.showOptionsMenu();
+			}
+		});
+		optionsBtn.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				SoundManager.menuBtnSound();
+			}
+		});
+		desktopPane.add(optionsBtn);
 		
-		JLabel lblNewLabel = new JLabel("Digger V0.1 - A project by Sebastian Z. & Armin P.");
+		JLabel lblNewLabel = new JLabel("Digger V1.0 - A project by Sebastian Z. & Armin P.");
 		lblNewLabel.setFont(customFontSmall);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 246, 490, 14);
+		lblNewLabel.setBounds(10, 248, 490, 14);
 		desktopPane.add(lblNewLabel);
 		
 		mainFrame.setVisible(true);
--- Digger/src/mainGame/MenuManager.java
+++ Digger/src/mainGame/MenuManager.java
@@ -6,17 +6,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 
 public class MenuManager extends GameManager {
+	public static ImageIcon icon = new ImageIcon("./images/solid.png");
 	private static File saveFile = new File("savegame.bin");
+	private static JCheckBox musicCheckbox;
+	private static JSlider musicVolumeSlider;
+	private static JSlider soundsVolumeSlider;
+	private static JCheckBox soundsCheckbox;
 
 	public static JMenuBar getGameMenu() {
 		JMenuBar menu = new JMenuBar();
@@ -31,24 +39,22 @@ public class MenuManager extends GameManager {
 		JMenuItem save = new JMenuItem("Save");
 		save.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(saveFile.exists() && !saveFile.isDirectory()){
-			        System.out.println(saveFile + " exists");
+				if (saveFile.exists() && !saveFile.isDirectory()) {
+					System.out.println(saveFile + " exists");
 					confirmSave();
-			       }
-				else {
-					if(save(player)) {
+				} else {
+					if (save(player)) {
 						System.out.println("Successfully saved game");
-					}
-					else {
+					} else {
 						JDialog errorDialog = new JDialog();
 						errorDialog.setSize(200, 150);
 						errorDialog.setAlwaysOnTop(true);
-						
+
 						JLabel errorMsg = new JLabel("Error while saving game.");
 						errorMsg.setPreferredSize(new Dimension(180, 45));
 						errorMsg.setHorizontalAlignment(SwingConstants.CENTER);
 						errorDialog.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-						
+
 						JButton confirmBtn = new JButton("O.K.");
 						confirmBtn.setMaximumSize(new Dimension(50, 23));
 						confirmBtn.setMinimumSize(new Dimension(50, 50));
@@ -67,7 +73,13 @@ public class MenuManager extends GameManager {
 				}
 			}
 		});
-		JMenuItem returnBtn = new JMenuItem("Return to menu");
+		JMenuItem optionsBtn = new JMenuItem("Settings");
+		optionsBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showOptionsMenu();
+			}
+		});
+		JMenuItem returnBtn = new JMenuItem("Main menu");
 		returnBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				graphic.dispose();
@@ -82,47 +94,60 @@ public class MenuManager extends GameManager {
 		});
 		file.add(load);
 		file.add(save);
+		file.add(optionsBtn);
 		file.add(returnBtn);
 		file.add(close);
+
 		JMenu help = new JMenu("Help");
 		JMenuItem controls = new JMenuItem("Controls");
+		controls.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFrame helpControls = new JFrame();
+				helpControls.setIconImage(icon.getImage());
+				helpControls.setContentPane(new JLabel(new ImageIcon("./images/helpMenuMovement.png")));
+				helpControls.setSize(510, 210);
+				helpControls.setResizable(false);
+				helpControls.setLocationRelativeTo(null);
+				helpControls.setVisible(true);
+			}
+		});
 		JMenuItem blocks = new JMenuItem("Blocks");
 		help.add(controls);
 		help.add(blocks);
+
 		menu.add(file);
 		menu.add(help);
 		return menu;
 	}
-	
+
 	public static void confirmSave() {
 		JFrame saveConfirmFrame = new JFrame();
 		saveConfirmFrame.setSize(375, 60);
 		saveConfirmFrame.setUndecorated(true);
 		saveConfirmFrame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		JLabel saveConfirmText = new JLabel("Overwrite old savegame file?");
 		saveConfirmText.setPreferredSize(new Dimension(139, 50));
 		saveConfirmText.setMaximumSize(new Dimension(139, 50));
 		saveConfirmText.setHorizontalAlignment(SwingConstants.CENTER);
 		saveConfirmFrame.getContentPane().add(saveConfirmText);
-		
+
 		JButton yesBtn = new JButton("Yes");
 		yesBtn.setPreferredSize(new Dimension(100, 50));
 		yesBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(save(player)) {
+				if (save(player)) {
 					saveConfirmFrame.dispose();
-				}
-				else {
+				} else {
 					JDialog errorDialog = new JDialog();
 					errorDialog.setSize(200, 150);
 					errorDialog.setAlwaysOnTop(true);
-					
+
 					JLabel errorMsg = new JLabel("Error while saving game.");
 					errorMsg.setPreferredSize(new Dimension(180, 45));
 					errorMsg.setHorizontalAlignment(SwingConstants.CENTER);
 					errorDialog.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-					
+
 					JButton confirmBtn = new JButton("O.K.");
 					confirmBtn.setMaximumSize(new Dimension(50, 23));
 					confirmBtn.setMinimumSize(new Dimension(50, 50));
@@ -141,7 +166,7 @@ public class MenuManager extends GameManager {
 			}
 		});
 		saveConfirmFrame.getContentPane().add(yesBtn);
-		
+
 		JButton noBtn = new JButton("No");
 		noBtn.setPreferredSize(new Dimension(100, 50));
 		noBtn.addActionListener(new ActionListener() {
@@ -150,10 +175,73 @@ public class MenuManager extends GameManager {
 			}
 		});
 		saveConfirmFrame.getContentPane().add(noBtn);
-		
+
 		saveConfirmFrame.setVisible(true);
 		saveConfirmFrame.setLocationRelativeTo(null);
 		saveConfirmFrame.setAlwaysOnTop(true);
 	}
 
+	public static void showOptionsMenu() {
+		JFrame optionFrame = new JFrame();
+		optionFrame.setSize(450, 295);
+		optionFrame.setTitle("Digger - Options");
+		optionFrame.getContentPane().setLayout(null);
+
+		musicCheckbox = new JCheckBox("Enabled");
+		musicCheckbox.setSelected(musicOn);
+		musicCheckbox.setBounds(106, 7, 97, 26);
+		optionFrame.getContentPane().add(musicCheckbox);
+
+		musicVolumeSlider = new JSlider(0, 100, musicSliderValue);
+		musicVolumeSlider.setBounds(224, 7, 200, 26);
+		optionFrame.getContentPane().add(musicVolumeSlider);
+
+		JLabel musicLabel = new JLabel("Music");
+		musicLabel.setBounds(23, 7, 46, 26);
+		optionFrame.getContentPane().add(musicLabel);
+
+		JLabel soundLabel = new JLabel("Sounds");
+		soundLabel.setBounds(23, 44, 46, 26);
+		optionFrame.getContentPane().add(soundLabel);
+
+		soundsCheckbox = new JCheckBox("Enabled");
+		soundsCheckbox.setSelected(soundsOn);
+		soundsCheckbox.setBounds(106, 43, 97, 26);
+		optionFrame.getContentPane().add(soundsCheckbox);
+
+		soundsVolumeSlider = new JSlider(0, 100, soundsSliderValue);
+		soundsVolumeSlider.setBounds(224, 44, 200, 26);
+		optionFrame.getContentPane().add(soundsVolumeSlider);
+		
+		JButton saveBtn = new JButton("Apply");
+		saveBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//saving variables for better usability
+				musicOn = musicCheckbox.isSelected();
+				soundsOn = soundsCheckbox.isSelected();
+				musicSliderValue = musicVolumeSlider.getValue();
+				soundsSliderValue = soundsVolumeSlider.getValue();
+				
+				//applying user settings
+				if (musicCheckbox.isSelected()) {
+					float volume = (float) musicVolumeSlider.getValue();
+					SoundManager.setMusicVolume(volume / 1000);
+				} else {
+					SoundManager.setMusicVolume(0);
+				}
+				if (soundsCheckbox.isSelected()) {
+					float volume = (float) soundsVolumeSlider.getValue();
+					soundsVolume = volume / 1000;
+				} else {
+					soundsVolume = 0;
+				}
+			}
+		});
+		saveBtn.setBounds(10, 216, 414, 34);
+		optionFrame.getContentPane().add(saveBtn);
+		
+		optionFrame.setAlwaysOnTop(true);
+		optionFrame.setLocationRelativeTo(null);
+		optionFrame.setVisible(true);
+	}
 }
